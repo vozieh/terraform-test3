81ddbebe1a7902b70f97316f038e572c5d7bcf96
Bobdingha Mecaly <bobmecaly@Bobdinghas-MacBook-Pro.local>
2024-12-06 21:36:40 +0100

    add ec2 resource

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
